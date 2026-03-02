63bf8e3d792bc73d3eae614b533746396c3a8df0
Muhammad Haris <57320391+hariszulfiqar054@users.noreply.github.com>
2026-03-03 00:57:45 +0500

    Added back the if conditional file

---
+++ conditional-statements-if/bank.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+	currentBalance := 1000.0
+	for {
+
+		fmt.Println(`
+Welcome to the Bank!
+1. Deposit
+2. Withdraw
+3. Check Balance
+4. Exit
+`)
+		var choice int
+		fmt.Print("Enter your choice: ")
+		fmt.Scanln(&choice)
+		if choice == 1 {
+			depositData := 0
+			fmt.Print("Deposit Amount : ")
+			fmt.Scanln(&depositData)
+			currentBalance += float64(depositData)
+		} else if choice == 2 {
+			withdraw := 0
+			fmt.Print("Withdraw Amount : ")
+			fmt.Scanln(&withdraw)
+			currentBalance -= float64(withdraw)
+
+		} else if choice == 3 {
+
+			fmt.Printf("Your balance : %v\n", currentBalance)
+		} else if choice == 4 {
+			fmt.Println("Exiting...")
+			break
+		} else {
+			fmt.Println("Invalid choice. Please try again.")
+		}
+	}
+}
