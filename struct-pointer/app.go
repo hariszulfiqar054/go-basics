761ed82ea5521482ac496f8498885f5584a97c5f
Muhammad Haris <57320391+hariszulfiqar054@users.noreply.github.com>
2026-03-10 00:27:37 +0500

    Added struct pointers

---
+++ struct-pointer/app.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type user struct {
+	firstName string
+	lastName  string
+	birthdate string
+	createdAt time.Time
+}
+
+func main() {
+	firstName := getUserData("Please enter your first name: ")
+	lastName := getUserData("Please enter your last name: ")
+	birthdate := getUserData("Please enter your birthdate (MM/DD/YYYY): ")
+
+	var userData user
+	userData = user{
+		firstName: firstName,
+		lastName:  lastName,
+		birthdate: birthdate,
+		createdAt: time.Now(),
+	}
+
+	outputUserData(&userData)
+
+	// Can also be written as below with the correct order
+	// userData = user{
+	// 	firstName,
+	// 	lastName,
+	// 	birthdate,
+	// 	time.Now(),
+	// }
+
+}
+
+func getUserData(promptText string) string {
+	fmt.Print(promptText)
+	var value string
+	fmt.Scan(&value)
+	return value
+}
+
+func outputUserData(userValue *user) {
+	// Go allow us to access the struct field directly without dereferencing the pointer
+	// Otherwise you need to something list this (*userValue).firstName
+	fmt.Println(userValue.firstName, userValue.lastName, userValue.birthdate, userValue.createdAt)
+}
